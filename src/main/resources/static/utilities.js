317dcc5c9c12c81238dff3a2abebce0cc2e0f4ce
duvanbarrera <duvan.barrera@mail.escuelaing.edu.co>
2015-11-26 13:53:05 -0500

    boton download sin funcionalidad

---
+++ src/main/resources/static/utilities.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function downloadLatex(){
+    var latex=document.getElementById("message").value;
+    alert(latex);
+    
+}
